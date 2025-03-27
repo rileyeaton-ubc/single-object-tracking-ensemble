2c274570fe08ba84fadc3d36cf5ed9de88452204
TheDarkKnight04 <santam040802@gmail.com>
2025-03-26 21:46:59 -0400

    Implemented bounding box to select object to be tracked

--- src/DSST/tracker.m
+++ src/DSST/tracker.m
@@ -1,7 +1,7 @@
 function dsst_tracker(path, initRect)
     % videoPath: path to folder with image frames
     % initRect: [x, y, width, height]
-
+    
     %% Parameters
     padding = 1.5;
     output_sigma_factor = 1/16; % For the Gaussian defined for the roi
@@ -11,7 +11,7 @@ function dsst_tracker(path, initRect)
     scale_model_max_area = 512; % Limits the tracking window size
     lambda = 1e-4; % For regularization to keep weights small
     learning_rate = 0.025;
-
+    
     %% Load frame files
     valid_exts = {'*.jpg','*.png',};
     frames = [];
@@ -25,10 +25,10 @@ function dsst_tracker(path, initRect)
     frames = frames(idx);
     numFrames = length(frames);
     getFrame = @(i) imread(fullfile(path, frames(i).name));
-
+    
     %fprintf('Loaded %d frames from: %s\n', numFrames, path);
     %disp({frames.name});
-
+    
     %% Load first frame
     im = getFrame(1);
     if size(im,3) > 1
@@ -37,6 +37,38 @@ function dsst_tracker(path, initRect)
     else
         im_disp = im;
     end
+    %% Select initial bounding box if not provided
+    if nargin < 2 || isempty(initRect)
+       figure; 
+       imshow(im_disp);
+       title('Draw initial bounding box, then press Enter');
+       h = imrect;
+       initRect = round(wait(h)); % [x y w h]
+       close;
+    end
+    %% Initialization
+    pos = [initRect(2) + initRect(4)/2, initRect(1) + initRect(3)/2]; % [y, x]
+    target_sz = [initRect(4), initRect(3)];
+    window_sz = floor(target_sz * (1 + padding));
+    output_sigma = sqrt(prod(target_sz)) * output_sigma_factor;
+    yf = fft2(gaussian_shaped_labels(output_sigma, window_sz));
+    cos_window = hann(window_sz(1)) * hann(window_sz(2))';
+    % Scale init
+    scale_factors = scale_step.^((1:num_scales) - ceil(num_scales/2));
+    scale_model_sz = target_sz;
+    if prod(scale_model_sz) > scale_model_max_area
+       scale_model_factor = sqrt(scale_model_max_area / prod(scale_model_sz));
+       scale_model_sz = floor(scale_model_sz * scale_model_factor);
+    else
+       scale_model_factor = 1.0;
+    end
+    scale_sigma = num_scales * scale_sigma_factor;
+    ss = (1:num_scales) - ceil(num_scales/2);
+    ys = exp(-0.5 * (ss.^2) / scale_sigma^2);
+    ysf = fft(ys);
+    scale_window = hann(num_scales)';
+    base_target_sz = target_sz;
+    currentScaleFactor = 1.0;
 end
 
 function [sorted, idx] = sort_nat(cellArray)
